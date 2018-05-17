24f51cf87d6599cb750c20e2ee132a65810bee8f
robear22890 <robgilmore127@gmail.com>
2018-05-17 15:14:49 -0500

    Added a function for combining article data with journal data.

--- R/articles.R
+++ R/articles.R
@@ -34,6 +34,8 @@ get_article_data <- function(queries, limit=7500, min_year=2008, max_year=2018, 
   message(sprintf("%s records passed the filter.", length(rownames(eps))))
   eps$journalIssn <- stringr::str_replace(eps$journalIssn, "x", "X")
   eps$journalIssn <- stringr::str_replace_all(eps$journalIssn, "-", "")
+  issns_df <- as.data.frame(stringr::str_split(eps$journalIssn, "; ", simplify = TRUE, n=3))
+  eps <- dplyr::mutate(eps, ISSN.1 = issns_df$V1, ISSN.2 = issns_df$V2)
   print(eps)
   return(eps)
 }
@@ -46,3 +48,45 @@ get_unique_issns <- function(issns){
   #issns <- unique(issns)
   return(issns)
 }
+
+issn2article_data <- function(data, issns) {
+  data <- dplyr::filter(data, (ISSN.1 %in% issns) | (ISSN.2 %in% issns))
+  return(data)
+}
+
+get_articles_with_journal_data <- function(article_data, journal_data) {
+  new_df <- NULL
+  for (i in 1:length(article_data$id)){
+    I1 <- as.character(article_data$ISSN.1[i])
+    I2 <- as.character(article_data$ISSN.2[i])
+    a_data <- article_data[i,]
+
+    if (I1 %in% journal_data$ISSN.1){
+      j_data <- dplyr::filter(journal_data, ISSN.1 == I1)
+      ja_data <- dplyr::left_join(a_data, j_data, by="ISSN.1")
+    } else if (I1 %in% journal_data$ISSN.2) {
+      j_data <- dplyr::filter(journal_data, ISSN.2 == I1)
+      ja_data <- dplyr::left_join(a_data, j_data, by=c("ISSN.1" = "ISSN.2"))
+    } else if (I2 %in% journal_data$ISSN.1) {
+      j_data <- dplyr::filter(journal_data, ISSN.1 == I2)
+      ja_data <- dplyr::left_join(a_data, j_data, by=c("ISSN.2" = "ISSN.1"))
+    } else if (I2 %in% journal_data$ISSN.2) {
+      j_data <- dplyr::filter(journal_data, ISSN.2 == I2)
+      ja_data <- dplyr::left_join(a_data, j_data, by="ISSN.2")
+    } else {
+      stop('ISSN didn not match')
+    }
+    if (is.null(new_df)){
+      new_df <- ja_data
+    } else{
+      new_df <- dplyr::bind_rows(new_df, ja_data)
+    }
+  }
+  return(new_df)
+}
+
+
+
+
+
+
