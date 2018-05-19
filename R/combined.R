6dcb0541b7ec9ece97eeecefda5ce502a5a4762b
sdhutchins <sdhutchins@outlook.com>
2018-05-19 15:54:37 -0500

    Moved R scripts to R folder.

--- R/combined.R
+++ R/combined.R
@@ -1,3 +1,15 @@
+#' @title Get Publication Data
+#'
+#' @description This function uses a journal source to combine data based on queries.
+#'
+#' @param journal_source The journal to search
+#' @param queries Use one or multiple queries
+#' @param limit A minimum number of articles
+#' @param min_year Minimum year to search
+#' @param max_year Maximum year to search
+#' @param min_citations Minimum number of citations in a journal
+#' @return A list of data objects including journals, articles, and combined data.
+#' @export
 get_publication_data <- function(journal_source, queries, limit=7500,
                                  min_year=2008, max_year=2018, min_citations=5) {
   hot_data <- list()
@@ -13,12 +25,12 @@ get_publication_data <- function(journal_source, queries, limit=7500,
   article_issns <- get_unique_issns(article_data$journalIssn)
   
   # Get journal data matching article ISSNs
-  journal_data <- issn2journal_data(data = journal_data, issns = article_issns)
+  journal_data <- issn_to_journal_data(data = journal_data, issns = article_issns)
   journal_issns <- get_unique_issns(journal_data$ISSN)
   hot_data[["journals"]] <- journal_data
   
   # Get article data mathcing available ISSNs
-  article_data <- issn2article_data(article_data, journal_issns)
+  article_data <- issn_to_article_data(article_data, journal_issns)
   hot_data[["articles"]] <- article_data
   
   # Combine article data with journal data
